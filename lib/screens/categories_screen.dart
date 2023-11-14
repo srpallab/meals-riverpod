aca526ae87ed369f9d9a45b1d3f1a48a3f03ecff
Shafiqur Rahman <pallab.alif@gmail.com>
2023-11-14 22:30:21 +0600

    ADD: Explicit Animation.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,8 +5,34 @@ import '../models/category.dart';
 import '../widgets/category_grid_item.dart';
 import 'meals_screen.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +48,42 @@ class CategoriesScreen extends StatelessWidget {
       );
     }
 
-    return GridView.builder(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (BuildContext context, Widget? child) => SlideTransition(
+        position: _animationController.drive(
+          Tween(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),
+          ),
+        ),
+        child: child,
+        // child: Padding(
+        //   padding: EdgeInsets.only(
+        //   top: 100 - _animationController.value * 100),
+        //   child: child,
+        // ),
       ),
-      itemCount: availableCategories.length,
-      itemBuilder: (BuildContext context, int index) {
-        return InkWell(
-          onTap: () {
-            onCategorySelected(context, availableCategories[index]);
-          },
-          splashColor: Theme.of(context).primaryColor,
-          borderRadius: BorderRadius.circular(15),
-          child: CategoryGridItem(category: availableCategories[index]),
-        );
-      },
+      child: GridView.builder(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemCount: availableCategories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () {
+              onCategorySelected(context, availableCategories[index]);
+            },
+            splashColor: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(15),
+            child: CategoryGridItem(category: availableCategories[index]),
+          );
+        },
+      ),
     );
   }
 }
